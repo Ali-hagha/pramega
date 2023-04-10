8a7113f3a19f459a9446a97433fa70dadeb4a0d4
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-04-10 19:10:13 +0330

    add avatars to hero section

--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -1,10 +1,12 @@
+import CustomerAvatars from '@/components/ui/CustomerAvatars/CustomerAvatars';
 import Image from 'next/image';
 import Link from 'next/link';
+import { RiStarFill } from 'react-icons/ri';
 
 const Hero = () => {
   return (
     <div className='mb-36 bg-neutral-light rounded-3xl flex justify-center items-center px-6 md:px-10 lg:px-0 lg:pl-16 xl:pl-24 overflow-hidden bg-[url("https://dummyimage.com/1000x1000/e8d8b2/e8d8b2")] bg-cover bg-center lg:bg-none'>
-      <div className="py-16 md:py-20 lg:pr-20 flex-1  ">
+      <div className="py-12 md:py-16 lg:pr-16 flex-1 basis-6/12 flex flex-col ">
         <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl leading-tight font-bold mb-6 md:mb-8 uppercase">
           Elevate Your Space
         </h1>
@@ -16,12 +18,19 @@ const Hero = () => {
         </p>
         <Link
           href="#"
-          className="inline-block px-10 py-3 bg-neutral-dark rounded-full text-neutral-light hover:bg-neutral-dark/80 transition-colors"
+          className="self-start block px-10 py-3 mb-12 md:mb-16 bg-neutral-dark rounded-full text-neutral-light hover:bg-neutral-dark/80 transition-colors"
         >
           Explore Products
         </Link>
+        <div className="self-start flex justify-center items-center ">
+          <CustomerAvatars />
+          <RiStarFill className="ml-4 text-[#f9a825]" />
+          <div className="ml-1 text-xs md:text-sm  text-neutral-dark/80">
+            4.6 (5.2k reviews)
+          </div>
+        </div>
       </div>
-      <div className="flex-1 self-stretch hidden lg:block">
+      <div className="flex-1 basis-5/12 self-stretch hidden lg:block">
         <Image
           width={1000}
           height={1000}
