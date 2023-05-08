70c8103ee5b5e914c0c5f519035a01847a3fcb56
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-05-08 19:48:57 +0330

    create products page sidebar

---
+++ components/ui/ProductsSidebar/ProductsSidebar.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { RiArrowRightSLine } from 'react-icons/ri';
+
+const ProductsSidebar = () => {
+  return (
+    <div className="w-64 shrink-0 mr-8 hidden lg:block">
+      <h3 className=" text-2xl font-bold mb-3">Categories</h3>
+      <ul className="ml-2 pl-2 border-l-gray-200 border-l-2">
+        <CategoryListItem category={'chairs'} path={'/chairs'} />
+        <CategoryListItem category={'lamps'} path={'/lamps'} />
+        <CategoryListItem category={'beds'} path={'/beds'} />
+        <CategoryListItem category={'sofas'} path={'/sofas'} />
+        <CategoryListItem category={'table'} path={'/table'} />
+      </ul>
+    </div>
+  );
+};
+
+const CategoryListItem = ({
+  category,
+  path,
+}: {
+  category: string;
+  path: string;
+}) => {
+  return (
+    <li className="mb-1">
+      <Link
+        href={`products${path}`}
+        className="flex group items-center justify-between font-medium text-neutral-dark/70 hover:text-neutral-dark hover:bg-gray-200 rounded-md  w-full py-1 px-2 transition-colors"
+      >
+        <p className="capitalize">{category}</p>
+        <RiArrowRightSLine className="text-xl relative opacity-0 group-hover:opacity-100 transition-all right-2 group-hover:right-0 " />
+      </Link>
+    </li>
+  );
+};
+
+export default ProductsSidebar;
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,6 @@
 import Crumbs from '@/components/ui/Crumbs/Crumbs';
 import ProductCard from '@/components/ui/ProductCard/ProductCard';
+import ProductsSidebar from '@/components/ui/ProductsSidebar/ProductsSidebar';
 import React from 'react';
 
 const products = [
@@ -14,12 +15,10 @@ const products = [
 ];
 const index = () => {
   return (
-    <div className="   mb-20">
-      <Crumbs />
+    <div className="mb-20">
+      <Crumbs classes="" />
       <div className="flex mt-6">
-        <div className="bg-red-300 w-72 shrink-0 mr-6 hidden lg:block">
-          sidebar
-        </div>
+        <ProductsSidebar />
         <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-5 flex-1">
           <ProductCard product={products[0]} />
           <ProductCard product={products[0]} />
