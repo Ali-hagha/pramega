d3f4a37f57ebc182f5d2d227e7b958582dc3e6b8
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-05-23 18:46:22 +0330

    move cart logic and state to separate hook

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,7 @@ import Footer from './ui/Footer/Footer';
 import BottomNavigation from './ui/BottomNavigation/BottomNavigation';
 import CartSidebar from './ui/CartSidebar/CartSidebar';
 import CartContext from '../context/CartContext';
+import { useCart } from '@/hooks/useCart';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -20,73 +21,14 @@ const bebas_neue = Bebas_Neue({
   style: 'normal',
 });
 
-const initProducts = [
-  {
-    id: '12345abcd',
-    name: 'Joshua Chair',
-    category: 'chairs',
-    price: 249,
-    rating: 4.5,
-    ratingCount: 629,
-    dimensions: {
-      width: 50,
-      depth: 53,
-      height: 80,
-    },
-    description:
-      'The popular Joshua is a mid-century inspired collection with a curved seat and back, designed for maximal comfort.',
-    imageUrlPrimary: '/categories/chair_tp.png',
-    imageUrlSecondary: '/products/chairs/Joshua-2.jpg',
-    productTag: 'new',
-    imageGalleryUrl: [
-      '/categories/chair_tp.png',
-      '/categories/lamp_tp.png',
-      '/categories/bed_tp.png',
-    ],
-  },
-];
-
 const Layout = ({ children }: { children: ReactNode }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartProducts, setCartProducts] = useState<Product[]>(initProducts);
-  const [cartProductCount, setCartProductCount] = useState<Map<string, number>>(
-    new Map([['12345abcd', 1]])
-  );
-
-  const addToCart = (product: Product, count: number) => {
-    // don't add a product to the products array if it already exists
-    if (!cartProducts.some(p => p.id === product.id)) {
-      const updatedProducts = [...cartProducts, product];
-      setCartProducts(updatedProducts);
-    }
-
-    const updatedCounts = new Map(cartProductCount);
-    if (updatedCounts.has(product.id)) {
-      updatedCounts.set(product.id, updatedCounts.get(product.id)! + count);
-    } else {
-      updatedCounts.set(product.id, count);
-    }
-
-    setCartProductCount(updatedCounts);
-  };
-
-  const toggleCart = () => {
-    setIsCartOpen(prevState => !prevState);
-  };
-
-  const cartContextValue: CartContextValue = {
-    toggleCart,
-    isCartOpen,
-    cartProducts,
-    cartProductCount,
-    addToCart,
-  };
+  const cartData = useCart();
 
   return (
     <div
       className={`${montserrat.variable} ${bebas_neue.variable} font-sans  text-neutral-dark relative`}
     >
-      <CartContext.Provider value={cartContextValue}>
+      <CartContext.Provider value={cartData}>
         <div className="px-6 md:px-8 xl:px-16">
           <Navbar />
           <main className="pt-20 md:pt-24">{children}</main>
---
+++ hooks/useCart.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+
+const initProducts = [
+  {
+    id: '12345abcd',
+    name: 'Joshua Chair',
+    category: 'chairs',
+    price: 249,
+    rating: 4.5,
+    ratingCount: 629,
+    dimensions: {
+      width: 50,
+      depth: 53,
+      height: 80,
+    },
+    description:
+      'The popular Joshua is a mid-century inspired collection with a curved seat and back, designed for maximal comfort.',
+    imageUrlPrimary: '/categories/chair_tp.png',
+    imageUrlSecondary: '/products/chairs/Joshua-2.jpg',
+    productTag: 'new',
+    imageGalleryUrl: [
+      '/categories/chair_tp.png',
+      '/categories/lamp_tp.png',
+      '/categories/bed_tp.png',
+    ],
+  },
+];
+
+export const useCart = (): CartContextValue => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartProducts, setCartProducts] = useState<Product[]>(initProducts);
+  const [cartProductCount, setCartProductCount] = useState<Map<string, number>>(
+    new Map([['12345abcd', 1]])
+  );
+
+  const addToCart = (product: Product, count: number) => {
+    // don't add a product to the products array if it already exists
+    if (!cartProducts.some(p => p.id === product.id)) {
+      const updatedProducts = [...cartProducts, product];
+      setCartProducts(updatedProducts);
+    }
+
+    const updatedCounts = new Map(cartProductCount);
+    if (updatedCounts.has(product.id)) {
+      updatedCounts.set(product.id, updatedCounts.get(product.id)! + count);
+    } else {
+      updatedCounts.set(product.id, count);
+    }
+
+    setCartProductCount(updatedCounts);
+  };
+
+  const removeFromCart = (product: Product) => {
+    setCartProducts(prevState => prevState.filter(p => p.id !== product.id));
+
+    const updatedCounts = new Map(cartProductCount);
+    if (updatedCounts.has(product.id)) {
+      updatedCounts.delete(product.id);
+    }
+
+    setCartProductCount(updatedCounts);
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen(prevState => !prevState);
+  };
+
+  return {
+    toggleCart,
+    isCartOpen,
+    cartProducts,
+    cartProductCount,
+    addToCart,
+    removeFromCart,
+  };
+};
