e629c748cab41542448181b7072bbfbdb4faa2b5
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-06-19 18:59:30 +0330

    show progress bar when navigating

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
+import NextNProgress from 'nextjs-progressbar';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,5 +16,14 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page);
 
-  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
+  return (
+    <Layout>
+      <NextNProgress
+        color="#d9f104"
+        height={5}
+        options={{ showSpinner: false }}
+      />
+      {getLayout(<Component {...pageProps} />)}
+    </Layout>
+  );
 }
