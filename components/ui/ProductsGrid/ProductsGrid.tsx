0084d1f48232e117e008a0e22e1f186fb132a332
Ali home pc <ali.haghayegh@gmail.com>
2023-06-28 10:25:45 +0330

    add max w to grid

--- components/ui/ProductsGrid/ProductsGrid.tsx
+++ components/ui/ProductsGrid/ProductsGrid.tsx
@@ -15,10 +15,12 @@ const ProductsGrid = ({ products }: Props) => {
     );
   }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-5 flex-1">
-      {products.map(product => {
-        return <ProductCard product={product} key={product.id} />;
-      })}
+    <div className="flex w-full justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-5 flex-1 2xl:max-w-[1200px]">
+        {products.map(product => {
+          return <ProductCard product={product} key={product.id} />;
+        })}
+      </div>
     </div>
   );
 };
