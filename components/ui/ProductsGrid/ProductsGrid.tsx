a381fd1020e910999b50645de03f9e09edc02d15
Ali home pc <ali.haghayegh@gmail.com>
2023-06-29 10:46:38 +0330

    show shimmmering card when data is being loaded

--- components/ui/ProductsGrid/ProductsGrid.tsx
+++ components/ui/ProductsGrid/ProductsGrid.tsx
@@ -1,21 +1,37 @@
 import ProductCard from '../ProductCard/ProductCard';
+import ProductCardLoadingShimmer from '../ProductCardLoadingShimmer/ProductCardLoadingShimmer';
 
 type Props = {
   products: Product[];
+  isLoading?: boolean;
 };
 
-const ProductsGrid = ({ products }: Props) => {
-  if (products.length < 1) {
+const ProductsGrid = ({ products, isLoading }: Props) => {
+  if (isLoading) {
+    let shimmeringCards = [];
+    for (let i = 0; i < 3; i++) {
+      shimmeringCards.push(<ProductCardLoadingShimmer key={i} />);
+    }
+
     return (
-      <div className="flex items-center justify-center flex-1">
-        <h1 className="text-3xl">
-          No products found in this category. Please try again later.
-        </h1>
+      <div className={`flex w-full justify-center`}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-5 flex-1 2xl:max-w-[1200px]">
+          {shimmeringCards}
+        </div>
       </div>
     );
   }
+
+  if (products.length < 1 && !isLoading) {
+    return (
+      <div className="flex items-center justify-center flex-1">
+        <h1 className="text-3xl">No products found. Please try again later.</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex w-full justify-center">
+    <div className={`flex w-full justify-center`}>
       <div className="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-5 flex-1 2xl:max-w-[1200px]">
         {products.map(product => {
           return <ProductCard product={product} key={product.id} />;
