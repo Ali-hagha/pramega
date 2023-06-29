6dcde654b1fa4a8e3f40e6112fb78ba956168aeb
Ali home pc <ali.haghayegh@gmail.com>
2023-06-29 10:47:20 +0330

    set link to have query params based on selected filters

--- components/ui/ProductsSidebar/ProductsSidebar.tsx
+++ components/ui/ProductsSidebar/ProductsSidebar.tsx
@@ -1,51 +1,111 @@
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { RiArrowRightSLine } from 'react-icons/ri';
+import { useEffect, useState } from 'react';
+import ProductsSideBarItem from './ProductsSideBarItem';
 
 const ProductsSidebar = () => {
+  const { isReady, query } = useRouter();
+  const [category, setCategory] = useState<string | undefined>();
+  const [featured, setFeatured] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (isReady) {
+      if (typeof query.featured === 'string') {
+        setFeatured(query.featured);
+      }
+      if (typeof query.category === 'string') {
+        setCategory(query.category);
+      }
+    }
+  }, [isReady, query.category, query.featured]);
+
+  const categoryItemData = [
+    {
+      title: 'all',
+      href: `/products/`,
+      active: !category,
+    },
+    {
+      title: 'chairs',
+      href: `/products/chairs${
+        !!featured ? '/filter?featured=' + featured : ''
+      }`,
+      active: category === 'chairs',
+    },
+    {
+      title: 'storage',
+      href: `/products/storage${
+        !!featured ? '/filter?featured=' + featured : ''
+      }`,
+      active: category === 'storage',
+    },
+    {
+      title: 'beds',
+      href: `/products/beds${!!featured ? '/filter?featured=' + featured : ''}`,
+      active: category === 'beds',
+    },
+    {
+      title: 'sofas',
+      href: `/products/sofas${
+        !!featured ? '/filter?featured=' + featured : ''
+      }`,
+      active: category === 'sofas',
+    },
+    {
+      title: 'tables',
+      href: `/products/tables${
+        !!featured ? '/filter?featured=' + featured : ''
+      }`,
+      active: category === 'tables',
+    },
+  ];
+
+  const featureItemData = [
+    {
+      title: 'all',
+      href: `/products/${!!category ? category : ''}`,
+      active: !featured,
+    },
+    {
+      title: 'on sale',
+      href: `/products${!!category ? '/' + category : ''}/filter?featured=sale`,
+      active: featured === 'sale',
+    },
+    {
+      title: 'new arrivals',
+      href: `/products${!!category ? '/' + category : ''}/filter?featured=new`,
+      active: featured === 'new',
+    },
+  ];
+
   return (
-    <div className="w-64 shrink-0 mr-8 hidden lg:block self-start sticky top-32">
-      <h3 className=" text-2xl font-bold mb-3">Categories</h3>
-      <ul className="ml-2 pl-2 border-l-gray-200 border-l-2">
-        <CategoryListItem category={'all'} href={'/products'} />
-        <CategoryListItem category={'chairs'} href={'/products/chairs'} />
-        <CategoryListItem category={'storage'} href={'/products/storage'} />
-        <CategoryListItem category={'beds'} href={'/products/beds'} />
-        <CategoryListItem category={'sofas'} href={'/products/sofas'} />
-        <CategoryListItem category={'tables'} href={'/products/tables'} />
-      </ul>
+    <div className="w-64 shrink-0 mr-8 hidden lg:block self-start sticky top-32 space-y-12">
+      <div>
+        <h3 className=" text-2xl font-bold mb-3">Categories</h3>
+        <ul className="ml-2 pl-2 border-l-gray-200 border-l-2">
+          {categoryItemData.map(item => (
+            <ProductsSideBarItem
+              key={item.title}
+              title={item.title}
+              href={item.href}
+              active={item.active}
+            />
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3 className=" text-2xl font-bold mb-3">Featured</h3>
+        <ul className="ml-2 pl-2 border-l-gray-200 border-l-2">
+          {featureItemData.map(item => (
+            <ProductsSideBarItem
+              key={item.title}
+              title={item.title}
+              href={item.href}
+              active={item.active}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
-  );
-};
-
-const CategoryListItem = ({
-  category,
-  href,
-}: {
-  category: string;
-  href: string;
-}) => {
-  const router = useRouter();
-  const path = router.asPath;
-
-  return (
-    <li className="mb-1">
-      <Link
-        href={`${href}`}
-        className={`flex group items-center justify-between font-medium  hover:text-neutral-dark hover:bg-gray-100 rounded-md  w-full py-2 px-3 transition-colors ${
-          href === path
-            ? 'bg-gray-100 text-neutral-dark'
-            : 'bg-transparent text-neutral-dark/70'
-        }`}
-      >
-        <p className="capitalize">{category}</p>
-        <RiArrowRightSLine
-          className={`text-xl relative  group-hover:opacity-100 transition-all  group-hover:right-0 ${
-            href === path ? 'opacity-100 right-0' : 'opacity-0 right-2'
-          }`}
-        />
-      </Link>
-    </li>
   );
 };
 
