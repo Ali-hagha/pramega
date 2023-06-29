8a4e1876d8d5fa6c465ba0aebded373a37b70fa5
Ali home pc <ali.haghayegh@gmail.com>
2023-06-29 18:21:59 +0330

    add active styles to category items

--- components/sections/Categories/CategoryItem.tsx
+++ components/sections/Categories/CategoryItem.tsx
@@ -14,15 +14,15 @@ const CategoryItem = ({ imgSrc, title }: Props) => {
         alt={''}
         width={600}
         height={600}
-        className="object-contain w-full h-full select-none group-hover:scale-105 group-hover:drop-shadow-[0_35px_35px_rgba(30,31,26,0.45)] transition-all"
+        className="object-contain w-full h-full select-none group-hover:scale-105 group-hover:drop-shadow-[0_35px_35px_rgba(30,31,26,0.45)] group-active:scale-105 group-active:drop-shadow-[0_35px_35px_rgba(30,31,26,0.45)] transition-all"
       />
       <div className="flex justify-between w-full items-center ">
         <p className="text-lg sm:text-2xl font-medium sm:font-semibold select-none capitalize ">
           {title}
         </p>
         <div className="h-8 w-8 sm:h-10 sm:w-10 bg-neutral-dark rounded-full flex items-center justify-center overflow-hidden relative">
-          <RiArrowRightUpLine className="text-primary text-2xl sm:text-3xl group-hover:-translate-y-8 group-hover:translate-x-8 transition-transform " />
-          <RiArrowRightUpLine className="text-primary text-2xl sm:text-3xl absolute translate-y-8 -translate-x-8 group-hover:translate-y-0 group-hover:translate-x-0 transition-transform -z-0" />
+          <RiArrowRightUpLine className="text-primary text-2xl sm:text-3xl group-hover:-translate-y-8 group-hover:translate-x-8 transition-transform group-active:-translate-y-8 group-active:translate-x-8" />
+          <RiArrowRightUpLine className="text-primary text-2xl sm:text-3xl absolute translate-y-8 -translate-x-8 group-hover:translate-y-0 group-hover:translate-x-0 transition-transform group-active:translate-y-0 group-active:translate-x-0" />
         </div>
       </div>
     </>
