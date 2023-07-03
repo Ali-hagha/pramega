1c63f87dfbcea26e17fb9a7e9409ea260ee1f939
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-07-03 18:31:00 +0330

    refactor component: move styles to inner component

--- components/CategoryThumbnailGroup/CategoryThumbnail.tsx
+++ components/CategoryThumbnailGroup/CategoryThumbnail.tsx
@@ -1,14 +1,21 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { RiArrowRightUpLine } from 'react-icons/ri';
+import Hero from '../Hero/Hero';
 
 type Props = {
   imgSrc: string;
   title: string;
+  classes: string;
+  href: string;
 };
 
-const CategoryThumbnail = ({ imgSrc, title }: Props) => {
+const CategoryThumbnail = ({ imgSrc, title, href, classes }: Props) => {
   return (
-    <>
+    <Link
+      href={href}
+      className={`bg-neutral-light rounded-3xl flex  flex-col items-center justify-center p-4 sm:p-6 md:p-4 lg:p-6 group ${classes}`}
+    >
       <Image
         src={imgSrc}
         alt={''}
@@ -17,7 +24,7 @@ const CategoryThumbnail = ({ imgSrc, title }: Props) => {
         className="object-contain w-full h-full select-none group-hover:scale-105 group-hover:drop-shadow-[0_35px_35px_rgba(30,31,26,0.45)] group-active:scale-105 group-active:drop-shadow-[0_35px_35px_rgba(30,31,26,0.45)] transition-all"
       />
       <div className="flex justify-between w-full items-center ">
-        <p className="text-lg sm:text-2xl font-medium sm:font-semibold select-none capitalize ">
+        <p className="sm:text-xl lg:text-2xl font-semibold select-none capitalize ">
           {title}
         </p>
         <div className="h-8 w-8 sm:h-10 sm:w-10 bg-neutral-dark rounded-full flex items-center justify-center overflow-hidden relative">
@@ -25,7 +32,7 @@ const CategoryThumbnail = ({ imgSrc, title }: Props) => {
           <RiArrowRightUpLine className="text-primary text-2xl sm:text-3xl absolute translate-y-8 -translate-x-8 group-hover:translate-y-0 group-hover:translate-x-0 transition-transform group-active:translate-y-0 group-active:translate-x-0" />
         </div>
       </div>
-    </>
+    </Link>
   );
 };
 
--- components/CategoryThumbnailGroup/CategoryThumbnailGroup.tsx
+++ components/CategoryThumbnailGroup/CategoryThumbnailGroup.tsx
@@ -8,48 +8,36 @@ const CategoryThumbnailGroup = () => {
         top categories
       </h3>
       <div className="grid grid-cols-2 md:grid-cols-4 md:grid-rows-2 xl:grid-cols-5 lg:max-h-[600px]  gap-4 rounded-3xl ">
-        <Link
+        <CategoryThumbnail
+          imgSrc={'/categories/chair_tp.png'}
+          title={'chairs'}
           href={'/products/chairs'}
-          className="bg-neutral-light col-span-2 row-span-1  md:col-span-2 md:row-span-2 rounded-3xl flex  flex-col items-center justify-center p-4 sm:p-6 group"
-        >
-          <CategoryThumbnail
-            imgSrc={'/categories/chair_tp.png'}
-            title={'chairs'}
-          />
-        </Link>
-        <Link
+          classes="col-span-2 row-span-1 md:col-span-2 md:row-span-2"
+        />
+        <CategoryThumbnail
+          imgSrc={'/categories/storage_tp.png'}
+          title={'storage'}
           href={'/products/storage'}
-          className="group bg-neutral-light  md:col-span-1 md:row-span-1  rounded-3xl flex  flex-col items-center justify-center p-4 sm:p-6"
-        >
-          <CategoryThumbnail
-            imgSrc={'/categories/storage_tp.png'}
-            title={'storage'}
-          />
-        </Link>
-        <Link
+          classes="md:col-span-1 md:row-span-1"
+        />
+        <CategoryThumbnail
+          imgSrc={'/categories/bed_tp.png'}
+          title={'beds'}
           href={'/products/beds'}
-          className="group bg-neutral-light  md:col-span-1 md:row-span-1 xl:col-span-2 rounded-3xl  flex  flex-col items-center justify-center p-4 sm:p-6"
-        >
-          <CategoryThumbnail imgSrc={'/categories/bed_tp.png'} title={'beds'} />
-        </Link>
-        <Link
+          classes="md:col-span-1 md:row-span-1 xl:col-span-2"
+        />
+        <CategoryThumbnail
+          imgSrc={'/categories/sofa_tp.png'}
+          title={'sofas'}
           href={'/products/sofas'}
-          className="group bg-neutral-light  md:col-span-1 md:row-span-1 xl:col-span-2 rounded-3xl flex  flex-col items-center justify-center p-4 sm:p-6"
-        >
-          <CategoryThumbnail
-            imgSrc={'/categories/sofa_tp.png'}
-            title={'sofas'}
-          />
-        </Link>
-        <Link
+          classes="md:col-span-1 md:row-span-1 xl:col-span-2"
+        />
+        <CategoryThumbnail
+          imgSrc={'/categories/table_tp.png'}
+          title={'tables'}
           href={'/products/tables'}
-          className="group bg-neutral-light  md:col-span-1 md:row-span-1 rounded-3xl flex  flex-col items-center justify-center p-4 sm:p-6"
-        >
-          <CategoryThumbnail
-            imgSrc={'/categories/table_tp.png'}
-            title={'tables'}
-          />
-        </Link>
+          classes="md:col-span-1 md:row-span-1"
+        />
       </div>
     </div>
   );
