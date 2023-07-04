4760bd2462170ea4cafa67d299b8815fe28ce2af
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-07-04 16:38:22 +0330

    add comments

--- hooks/useCrumbs.tsx
+++ hooks/useCrumbs.tsx
@@ -3,7 +3,10 @@ import { useRouter } from 'next/router';
 export const useCrumbs = () => {
   const router = useRouter();
   const crumbsRaw = router.asPath.split('/');
+
+  // remove last element that is an empty string from array
   crumbsRaw.shift();
+  // remove query params
   const crumbs = crumbsRaw.filter(c => c !== '' && !c.includes('?'));
 
   let crumbPaths: string[] = [];
