b7186a941b7327ed72c40580ee41f741094c44eb
Ali home pc <ali.haghayegh@gmail.com>
2023-08-19 16:15:21 +0330

    fix anchor element size

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -27,46 +27,49 @@ const ProductCard = ({ product, touchEnabled = true }: Props) => {
   };
 
   return (
-    <Link
-      href={`/products/${product.attributes.category}/${product.attributes.productId}`}
-    >
-      <div className=" rounded-3xl  overflow-hidden border-4 border-solid border-gray-100 hover:border-gray-300 active:border-gray-300 group/card transition-colors">
-        <div className=" aspect-square relative  bg-gray-100">
-          {product.attributes.productTag && (
-            <ProductTag productTag={product.attributes.productTag} />
-          )}
-          <Images
-            baseImage={
-              backendUrl +
-              product.attributes.primaryImage.data.attributes.formats.medium.url
-            }
-            secondaryImage={
-              backendUrl +
-              product.attributes.secondaryImage.data.attributes.formats.medium
-                .url
-            }
-            touchEnabled={touchEnabled}
-          />
-        </div>
-        <div className="flex flex-col p-5  bg-white">
-          <p className="text-xl font-semibold  mb-4">
-            {product.attributes.name}
-          </p>
-          <div className="flex items-center justify-between w-full">
-            <div>
-              <p className="text-sm mb-1 text-neutral-dark/70">Price:</p>
-              <p className="text-xl font-semibold text-neutral-dark/90">
-                {currencyFormatter.format(product.attributes.price)}
-              </p>
-            </div>
-            <AddToCartBtn
-              onClick={() => handleAddToCart(product)}
-              productId={product.id}
+    <div>
+      <Link
+        href={`/products/${product.attributes.category}/${product.attributes.productId}`}
+      >
+        <div className=" rounded-3xl  overflow-hidden border-4 border-solid border-gray-100 hover:border-gray-300 active:border-gray-300 group/card transition-colors">
+          <div className=" aspect-square relative  bg-gray-100">
+            {product.attributes.productTag && (
+              <ProductTag productTag={product.attributes.productTag} />
+            )}
+            <Images
+              baseImage={
+                backendUrl +
+                product.attributes.primaryImage.data.attributes.formats.medium
+                  .url
+              }
+              secondaryImage={
+                backendUrl +
+                product.attributes.secondaryImage.data.attributes.formats.medium
+                  .url
+              }
+              touchEnabled={touchEnabled}
             />
           </div>
+          <div className="flex flex-col p-5  bg-white">
+            <p className="text-xl font-semibold  mb-4">
+              {product.attributes.name}
+            </p>
+            <div className="flex items-center justify-between w-full">
+              <div>
+                <p className="text-sm mb-1 text-neutral-dark/70">Price:</p>
+                <p className="text-xl font-semibold text-neutral-dark/90">
+                  {currencyFormatter.format(product.attributes.price)}
+                </p>
+              </div>
+              <AddToCartBtn
+                onClick={() => handleAddToCart(product)}
+                productId={product.id}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
